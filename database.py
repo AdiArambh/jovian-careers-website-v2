bfe9214d98cd54ccd6938c51679e4d25bf814787
Alpha Aditya <alphacodethunder@gmail+github.com>
2023-06-07 12:45:02 +0530

    added connection to db.

---
+++ database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine,text
+from dotenv.main import load_dotenv
+import os
+
+load_dotenv()
+
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(
+    db_connection_string,
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+        }
+    },
+)
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+
+    jobs = []
+    for row in result.all():
+      jobs.append(row._asdict())
+
+    return jobs
